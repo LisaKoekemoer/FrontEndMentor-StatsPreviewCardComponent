9080bebe0a57a82aa87a57f44de934a896acc988
Alicia <alicia.koekemoer98@gmail.com>
2024-06-05 20:49:54 +0200

    Completed Stats Preview Card Component Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import statsImage from './images/image-header-desktop.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='stats-card'>
+        <div className='stats-info'>
+          <h1>Get <span>insights</span> that help your business grow.</h1>
+          <p className='stats-par'>Discover the benefits of data analytics and make better 
+            decisions regarding revenue, customer experience, and overall efficiency.</p>
+          <div className='stats-numbers'>
+            <div className='companies'>
+              <h2>10k+</h2>
+              <p>COMPANIES</p>
+            </div>
+            <div className='templates'>
+              <h2>314</h2>
+              <p>TEMPLATES</p>
+            </div>
+            <div className='queries'>
+              <h2>12M+</h2>
+              <p>QUERIES</p>
+            </div>
+          </div>
+        </div>
+        <div className='stats-img'>
+          <img src={statsImage} alt=""/>
+        </div>
+      </div>
+    </>
   );
 }
 
